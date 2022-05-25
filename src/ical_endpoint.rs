5fef934a4241f1183300f78fe510f9cc3e819576
Jackson Ming Hu <huming2207@gmail.com>
2022-05-25 19:12:31 +1000

    handler: init

---
+++ src/ical_endpoint.rs
@@ -0,0 +1,36 @@
+use actix_web::{web, Responder, HttpResponse, http::header::ContentType};
+use serde::Deserialize;
+
+use crate::ical_json::CalendarItem;
+
+#[derive(Deserialize)]
+pub struct IcalRequest {
+    feed: String,
+    tzid: String,
+    sort: bool,
+    limit: usize,
+}
+
+pub async fn ical_handler(form: web::Json<IcalRequest>) -> impl Responder {
+    match CalendarItem::from_ical_url(&form.feed, form.tzid.clone(), &form.tzid, form.sort, form.limit).await {
+        Ok(ical) => {
+            match serde_json::to_string(&ical) {
+                Ok(str) => {
+                    return HttpResponse::Ok()
+                        .content_type(ContentType::json())
+                        .body(str)
+                }
+                Err(err) => {
+                    return HttpResponse::InternalServerError()
+                        .content_type(ContentType::plaintext())
+                        .body(format!("Failed to encode JSON, reason: {}", err.to_string()))
+                }
+            };
+        }
+        Err(err) => {
+            return HttpResponse::BadRequest()
+                .content_type(ContentType::plaintext())
+                .body(format!("Failed to convert, reason: {}", err.to_string()))
+        }
+    }
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod ical_json;
+mod ical_endpoint;
 mod error_type;
 use actix_web::{HttpServer, App, web};
+use ical_endpoint::ical_handler;
 
 #[tokio::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .route("/api/ical", web::post().to(ical_handler))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
